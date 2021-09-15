4ce498a7dd337a4a1875040be9eb831cdfa8275f
matthew chimenti <matthewosu@gmail.com>
2021-09-14 20:54:59 -0700

    have layout for future styling of event page made with divs and borders

--- natureproject/natureapp/models.py
+++ natureproject/natureapp/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
+    event_date = models.DateTimeField(default=now, blank=True)
 
     def __str__(self):
         return self.title 
