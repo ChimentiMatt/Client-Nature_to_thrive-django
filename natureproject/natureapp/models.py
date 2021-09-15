3c3a9bdc89878a43584f89a00691da0df1161fc9
matthew chimenti <matthewosu@gmail.com>
2021-09-15 13:55:28 -0700

    more building of the events page. Image is built in and not yeat from the post

--- natureproject/natureapp/models.py
+++ natureproject/natureapp/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     event_date = models.DateTimeField(default=now, blank=True)
+    image = models.ImageField(upload_to='images/', default='cat.jpg')
 
     def __str__(self):
         return self.title 
--- natureproject/natureapp/urls.py
+++ natureproject/natureapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-# from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from .views import EventView, HomeView, HeaderView, AboutView
 
 urlpatterns = [
-    # path('', views.home, name="home"),
     path('', HomeView, name="home"),
     path('header/', HeaderView),
     path('about/', AboutView, name="about"),
     path('events/', EventView.as_view(), name="events"),
-]
+] 
+
+urlpatterns += staticfiles_urlpatterns()
